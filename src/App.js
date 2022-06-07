83da633362fbf793d2a195f3d44c12c9815bfa3b
Ludwigson Morisseau <morisseau@Ludwigsons-MacBook-Air.local>
2022-06-07 16:28:29 -0400

    created the frontend to use jwt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import Login from './components/Login';
+import Home from './components/Home';
+import 'antd/dist/antd.css'
+// import './App.css';
 
 function App() {
+  const [token, setToken] = useState('');
+  useEffect(() => {
+    const _token = localStorage.getItem('token');
+    if (_token) {
+      setToken(_token);
+    }
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!token
+        ? <Login setToken={setToken} />
+        : <Home token={token} />
+      }
     </div>
   );
 }
